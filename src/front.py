b48d6a7383b34319579f362efd1068f590a8467b
OneCaua95 <cauawendel9@gmail.com>
2025-11-01 18:23:48 -0300

    Adicionando funções, criando grafico no streamlit, consertando analise de sentimento

--- src/front.py
+++ src/front.py
@@ -1,11 +1,30 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-
-file = 'data\Twitch_game_data.csv'
-data = pd.read_csv(file, encoding='latin-1')
-
 st.set_page_config(layout="wide")
+from extract import Extractor
 
-st.title('Projeto MetaVerso')
-st.sidebar.write("barra lateral")
+data = Extractor()
+
+def AnaliseDeSentimento():
+    st.title('Projeto MetaVerso')
+    st.sidebar.selectbox("barra lateral",("1","2"))
+
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+
+    with col1:
+        st.header("Garfico de Serntimento")
+        df = data.get_data()
+        st.bar_chart(df, x="Avg_viewer_ratio",y="Streamers")
+    with col2:
+        st.header("Top Comentarios")
+
+
+    with col3:
+        st.header("Usuarios Que mais Participam")
+
+    with col4:
+        st.header("Nuvem de Palavras")
+    return
+
+AnaliseDeSentimento()
--- src/sentiment.py
+++ src/sentiment.py
@@ -1,18 +1,18 @@
 from pysentimiento import create_analyzer
 
-analyzer = create_analyzer(model_name='pysentimiento/bertweet-pt-sentiment', lang='pt')
+py_analyzer = create_analyzer(task="sentiment", lang='pt')
 
 class Analyzer:
 
-    def sentiment_analyzer(text):
-        texto_analisado = analyzer.predict(text)
+    def sentiment_analyzer(self, text):
+        texto = text
+        texto_analisado = py_analyzer.predict(texto)
 
         return texto_analisado
-
-
-
+    
 # TESTE
 
 teste = Analyzer()
 
 print(teste.sentiment_analyzer("Jogo Ruim!"))
+
